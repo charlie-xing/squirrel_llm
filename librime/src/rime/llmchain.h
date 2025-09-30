7a64c039a531afed3a4fd0a92606e99c411dc3c3
charlie-xing <xcl@stu.pku.edu.cn>
2025-09-30 18:32:40 +0800

    Add LLM integration

---
+++ librime/src/rime/llmchain.h
@@ -0,0 +1,6 @@
+#ifndef LLMCHAIN_H
+#define LLMCHAIN_H
+#include <string>
+std::string py_generate(const std::string& url, const std::string& model, const std::string& prompt,const bool first_flag);
+
+#endif
